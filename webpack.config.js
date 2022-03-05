6647ca2bdffa1bc1e732b061fc0bf7eef599b508
Augusto Camargo <gutto88@gmail.com>
2022-03-05 01:38:30 -0300

    Servindo HTML estático

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 ﻿const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
@@ -12,6 +13,12 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
 
   module: {
     rules: [
