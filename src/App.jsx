97d9da403fd883967fd9feeebf7ccec9bf244bc2
Augusto Camargo <gutto88@gmail.com>
2022-03-06 20:58:58 -0300

    Importando arquivos CSS (DONE CONFIG)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-﻿export function App() {
-  throw new Error("omg errro")
-  return <h1>Hello Aueegusto</h1>
+﻿import './styles/global.css';
+
+export function App() {
+  //throw new Error("omg errro")
+  return <h1>Hello World and Augusto. This 'Hello' is powered by ReactJS. How do you feel about it?!</h1>
 }
