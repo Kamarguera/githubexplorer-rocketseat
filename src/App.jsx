cc4bfa627c13103f3bd26860ce75fd14fd2f3e12
Augusto Camargo <gutto88@gmail.com>
2022-03-13 12:13:43 -0300

    1st react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-﻿import './styles/global.scss';
+﻿import { RepositoryList } from './components/RepositoryList';
+import './styles/global.scss';
+
+// export function App() {
+//   //throw new Error("omg erro")
+//   return <h1>Hello World and Augusto. This 'Hello' is powered by ReactJS. How do you like that?</h1>
+// }
+
+
 
 export function App() {
-  //throw new Error("omg erro")
-  return <h1>Hello World and Augusto. This 'Hello' is powered by ReactJS. How do you like that?</h1>
+  return <RepositoryList/>
 }
+
